e0b5759d0033365a1e0f610c815062a4fed0b773
oladeleseyi <toothy61@gmail.com>
2021-10-08 17:16:39 +0100

    Hero

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Hero from "./Components/Hero";
 
 function App() {
   return (
     <div className="App">
+      <Hero />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
