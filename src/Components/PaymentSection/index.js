18274d811d014346ed11fa2bb472b99635b9723f
oladeleseyi <toothy61@gmail.com>
2021-10-09 12:20:53 +0100

    fix git error

--- src/Components/PaymentSection/index.js
+++ src/Components/PaymentSection/index.js
@@ -84,9 +84,7 @@ const PaymentSection = () => {
     try {
       getCurrentPrice().then((res) => {
         const { buyPricePerCoin, id } = res.data;
-
         console.log("background update", buyPricePerCoin);
-
         setCurrentPrice(buyPricePerCoin);
         setOrderId(id);
       });
